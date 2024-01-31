981076f9abc8ffd5d8ba7ada9b69ce0c50b20e33
samuel-722 <115673134+samuel-722@users.noreply.github.com>
2024-01-31 13:03:05 -0800

    lab1 python

--- Labs/Lab1/0131-Lab1.py
+++ Labs/Lab1/0131-Lab1.py
@@ -1,9 +1,14 @@
 
+def matchTarget(nums, target):
+    num_indeces = {} #create a dictionary
 
+    for i, num in enumerate(nums): #iterate through array along with its indice
+        match = target - num #calculate the match of each number that would get it to its target
+        if match in num_indeces: #check if match is in the dictionary
+            return [num_indeces[match], i] #if found, return index of the both numbers
+        num_indeces[num] = i
+    return[] #return nothing (an empty list) if nothing is found
 
-
-def addSum(nums, target):
-    num_indeces = {}
-
-    for i, num in enumerate(nums):
-        match = target - num
+nums = [0,1,2,3,4,5,6,7,8,9]
+target = 17
+print(matchTarget(nums, target))
