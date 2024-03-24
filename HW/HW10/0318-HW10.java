710b0522113e1e0ca55d1f5cddbc124e37635966
samuel-722 <115673134+samuel-722@users.noreply.github.com>
2024-03-24 11:29:29 -0700

    markdown

--- HW/HW10/0318-HW10.java
+++ HW/HW10/0318-HW10.java
@@ -1,5 +1,56 @@
-package HW.HW10;
+import java.util.List;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
-public class 0318-HW10 {
-    
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int value) {
+        val = value;
+        left = null;
+        right = null;
+    } //treenode
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                currentLevel.add(node.val);
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            } //for
+
+            result.add(currentLevel);
+        } //while
+
+        return result;
+    } //level order
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(1);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(9);
+
+        TreeNode tree = new TreeNode(3);
+        List<List<Integer>> traversal = tree.levelOrder(root);
+        System.out.println(traversal);
+    }
 }
