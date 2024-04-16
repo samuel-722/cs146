c6d14aeb6f409aaeffff962d4768c20783ff3c9f
samuel-722 <115673134+samuel-722@users.noreply.github.com>
2024-04-15 23:50:35 -0700

    hw12

---
+++ HW/HW12/0414-HW12.java
@@ -0,0 +1,5 @@
+package HW.HW12;
+
+public class 0414-HW12 {
+    
+}
---
+++ HW/HW12/0414-HW12.py
@@ -0,0 +1,12 @@
+
+
+
+def minCostToSupplyWater(self, n: int, wells: List[int], pipes: List[List[int]]) -> int:
+    # initialize a list to store all pipes
+    all_pipes = [[0, i+1, well_cost] for i, well_cost in enumerate(wells)]  # Represent wells as pipes from the water source (node 0) to each house
+    all_pipes.extend(pipes)
+
+    #sort pipes based on cost in non-decreasing order
+    all_pipes.sort(key = lambda x: x[2])
+    #
+
